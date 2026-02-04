1579d6e0d964b16f5a4c0bccbe9c4a179ca54793
Andre Santos <andressouza25@gmail.com>
2026-02-04 15:44:18 -0300

    feat: add sueAthContext hook

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,4 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
 import z from 'zod'
@@ -22,7 +21,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { AuthContext } from '@/context/auth'
+import { useAuthContext } from '@/context/auth'
 
 const loginSchema = z.object({
   email: z
@@ -40,7 +39,7 @@ const loginSchema = z.object({
 })
 
 const LoginPage = () => {
-  const { user, login } = useContext(AuthContext)
+  const { user, login } = useAuthContext()
 
   const form = useForm({
     resolver: zodResolver(loginSchema),
