03b8624f71255291f87be92db44a7fa2fcff3c12
Andre Santos <andressouza25@gmail.com>
2026-02-11 14:21:58 -0300

    feat: ensure date range can be selected on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
+import { PlusIcon } from 'lucide-react'
 import { Navigate } from 'react-router'
 
+import DateSelection from '@/components/data-selection'
 import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/context/auth'
 
 const HomePage = () => {
@@ -15,6 +18,19 @@ const HomePage = () => {
   return (
     <>
       <Header />
+      {/* TOPO */}
+      <div className="p-8">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold">Dashboard</h2>
+          <div className="flex items-center gap-2">
+            <DateSelection />
+            <Button>
+              <PlusIcon />
+              Nova Transação
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
