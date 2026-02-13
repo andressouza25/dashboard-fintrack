7338cc905088143657370823b79411ae878c4b8b
Andre Santos <andressouza25@gmail.com>
2026-02-13 17:41:51 -0300

    feat: add loadgin to signup button

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -58,17 +58,16 @@ export const AuthContextProvider = ({ children }) => {
     init()
   }, [])
 
-  const signup = (data) => {
-    signupMutation.mutate(data, {
-      onSuccess: (createdUser) => {
-        setUser(createdUser)
-        setTokens(createdUser.tokens)
-        toast.success('Conta criada com sucesso!')
-      },
-      onError: () => {
-        toast.error('Erro ao criar a conta. Por favor, tente novamente!')
-      },
-    })
+  const signup = async (data) => {
+    try {
+      const createdUser = await signupMutation.mutateAsync(data)
+      setUser(createdUser)
+      setTokens(createdUser.tokens)
+      toast.success('Conta criada com sucesso!')
+    } catch (error) {
+      toast.error('Erro ao criar a conta. Por favor, tente novamente!')
+      console.log(error)
+    }
   }
 
   const login = async (data) => {
@@ -78,8 +77,8 @@ export const AuthContextProvider = ({ children }) => {
       setTokens(loggedUser.tokens)
       toast.success('Login realizado com sucesso!')
     } catch (error) {
+      toast.error('Erro ao realizar login. Por favor, tente novamente!')
       console.log(error)
-      toast.error('Erro ao logar. Por favor, tente novamente!')
     }
   }
 
