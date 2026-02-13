c33c41153f5cf466706204bb8a1b3c8a3f4916ef
Andre Santos <andressouza25@gmail.com>
2026-02-13 17:58:12 -0300

    feat: add getAll function to transaction service

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -1,3 +1,5 @@
+import queryString from 'query-string'
+
 import { protectedApi } from '@/lib/axios'
 
 export const TransactionService = {
@@ -11,6 +13,18 @@ export const TransactionService = {
    */
   create: async (input) => {
     const response = await protectedApi.post('/transactions', input)
+    return response.data
+  },
+  /**
+   * Retorna as transações do usuário autenticado
+   * @param {Object} input
+   * @param {string} input.from - Data inicial (YYYY-MM-DD)
+   * @param {string} input.to - Data final (YYYY-MM-DD)
+   */
+  getAll: async (input) => {
+    const query = queryString.stringify({ from: input.from, to: input.to })
+    const response = await protectedApi.get(`/transactions?${query}`)
+
     return response.data
   },
 }
