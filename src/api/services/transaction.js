079e3859367dff09ee04a0b6c60cd2115a0b34a5
Andre Santos <andressouza25@gmail.com>
2026-02-16 14:32:25 -0300

    feat: add update transaction service and hooks functions

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -12,7 +12,12 @@ export const TransactionService = {
    * @param {string} input.type - Tipo da transação (EARNING/EXPENSE/INVESTMENT)
    */
   create: async (input) => {
-    const response = await protectedApi.post('/transactions', input)
+    const response = await protectedApi.post('/transactions', {
+      name: input.name,
+      date: input.date,
+      amount: input.amount,
+      type: input.type,
+    })
     return response.data
   },
   /**
@@ -25,6 +30,24 @@ export const TransactionService = {
     const query = queryString.stringify({ from: input.from, to: input.to })
     const response = await protectedApi.get(`/transactions?${query}`)
 
+    return response.data
+  },
+  /**
+   * Atualiza uma transação para o usuário autenticado
+   * @param {Object} input - Dados da transação.
+   * @param {Object} input.id - ID da transação.
+   * @param {string} input.name - Nome da transação
+   * @param {string} input.date - Data da transação (YYYY-MM-DD)
+   * @param {string} input.amount - Valor da transação
+   * @param {string} input.type - Tipo da transação (EARNING/EXPENSE/INVESTMENT)
+   */
+  update: async (input) => {
+    const response = await protectedApi.patch(`/transactions/${input.id}`, {
+      name: input.name,
+      date: input.date,
+      amount: input.amount,
+      type: input.type,
+    })
     return response.data
   },
 }
